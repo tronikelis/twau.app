0d05cc883ce8509ac2ab62901927636221d7bcc5
Tronikel <contactdonatas@gmail.com>
2025-04-19 10:19:01 +0300

    feat: delete room after a timeout

--- pkgs/game_state/rooms.go
+++ pkgs/game_state/rooms.go
@@ -1,7 +1,9 @@
 package game_state
 
 import (
+	"log"
 	"sync"
+	"time"
 
 	"word-amongus-game/pkgs/ws"
 )
@@ -36,15 +38,20 @@ func (self *Room) State(mutate func(state GameState) error) error {
 	return mutate(self.unsafeState)
 }
 
+type roomWithDeleteChan struct {
+	room             *Room // can be returned with a locked outer mutex
+	cancelDeleteChan chan struct{}
+}
+
 // concurrency safe
 type Rooms struct {
-	statesById map[string]*Room
+	statesById map[string]*roomWithDeleteChan
 	mu         *sync.Mutex
 }
 
 func NewRooms() Rooms {
 	return Rooms{
-		statesById: map[string]*Room{},
+		statesById: map[string]*roomWithDeleteChan{},
 		mu:         &sync.Mutex{},
 	}
 }
@@ -62,12 +69,16 @@ func (self Rooms) Room(roomId string) (*Room, bool) {
 	self.mu.Lock()
 	defer self.mu.Unlock()
 
-	game, ok := self.statesById[roomId]
+	room, ok := self.statesById[roomId]
 	if !ok {
 		return nil, false
 	}
 
-	return game, true
+	if room.cancelDeleteChan != nil {
+		room.cancelDeleteChan <- struct{}{}
+	}
+
+	return room.room, true
 }
 
 // creates a room, or returns (zero, false) if room exists
@@ -80,14 +91,41 @@ func (self Rooms) CreateRoom(roomId string) (*Room, bool) {
 	defer self.mu.Unlock()
 
 	room := NewRoom()
-	self.statesById[roomId] = room
+	self.statesById[roomId] = &roomWithDeleteChan{
+		room: room,
+	}
 
 	return room, true
 }
 
-func (self Rooms) DeleteRoom(roomId string) {
+func (self Rooms) QueueDelete(roomId string) {
 	self.mu.Lock()
 	defer self.mu.Unlock()
+
+	room, ok := self.statesById[roomId]
+	if !ok {
+		return
+	}
+
+	cancelDeleteChan := make(chan struct{})
+	room.cancelDeleteChan = cancelDeleteChan
+
+	go func() {
+		select {
+		case <-time.After(time.Second * 30):
+			self.deleteRoom(roomId)
+			return
+		case <-cancelDeleteChan:
+			return
+		}
+	}()
+}
+
+func (self Rooms) deleteRoom(roomId string) {
+	self.mu.Lock()
+	defer self.mu.Unlock()
+
+	log.Println("deleting room", roomId)
 
 	delete(self.statesById, roomId)
 }
